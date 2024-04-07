b4694a0b44fba357482e988f1c069d3aa97536c8
Leonardo <leonardo.yakub@gmail.com>
2024-04-07 17:35:36 +0200

    correcting localhost port for api route url in development

--- trpc/Provider.tsx
+++ trpc/Provider.tsx
@@ -16,7 +16,7 @@ function getBaseUrl() {
     // reference for vercel.com
     return `https://${process.env.VERCEL_URL}`;
   // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+  return `http://localhost:${process.env.PORT ?? 8081}`;
 }
 
 export function TRPCReactProvider({ children }: { children: React.ReactNode }) {
