c08be22f3e261352b2351546f692cc0c796bcb95
Leonardo <leonardo.yakub@gmail.com>
2024-04-26 14:05:05 +0200

    removed headerTransparent option fix with new New Architecture

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -2,16 +2,12 @@ import { Stack } from "expo-router";
 
 import { useAuth } from "@clerk/clerk-expo";
 
-import { NAV_THEME } from "@/lib/constants/Colors";
-import { useColorScheme } from "@/lib/hooks/useColorScheme";
-
 export const unstable_settings = {
   initialRouteName: "(tabs)",
 };
 
 export default function RootLayout() {
   const { isSignedIn } = useAuth();
-  const { isDarkColorScheme } = useColorScheme();
 
   return (
     <Stack
@@ -28,13 +24,6 @@ export default function RootLayout() {
         name="sign-in"
         redirect={isSignedIn}
         options={{
-          // onPress only works if header position is set to absolute (headerTransparent: true)
-          headerTransparent: true,
-          headerStyle: {
-            backgroundColor: isDarkColorScheme
-              ? NAV_THEME.dark.card
-              : NAV_THEME.light.card,
-          },
           presentation: "modal",
           title: "Sign-In",
         }}
