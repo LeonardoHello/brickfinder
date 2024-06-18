b197603a285ee351bb55299892264e5499dfc27f
Leonardo <leonardo.yakub@gmail.com>
2024-06-18 20:11:45 +0200

    removed ProfileFormInputs component

--- components/ProfileForm.tsx
+++ components/ProfileForm.tsx
@@ -1,19 +1,24 @@
 import { useMemo, useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
+import { Asterisk } from "@tamagui/lucide-icons";
+import "tamagui";
 import {
   Button,
+  Fieldset,
   Form,
+  Input,
   Label,
   SizableText,
   Spinner,
   Theme,
   YStack,
 } from "tamagui";
+import isEmail from "validator/lib/isEmail";
+import isMobilePhone from "validator/lib/isMobilePhone";
 import { z } from "zod";
 
 import ProfileFormFieldArray from "./ProfileFormFieldArray";
-import ProfileFormInputs from "./ProfileFormInputs";
 import { JobSchema, UserSchema } from "@/lib/db/schema";
 import { trpc } from "@/lib/utils/trpc";
 import { RouterOutputs } from "@/trpc/routers";
@@ -34,7 +39,7 @@ const FormSchema = z.object({
 export type FormSchema = typeof FormSchema._type;
 
 export default function ProfileForm({
-  currentUser: { id, updatedAt, ...rest },
+  currentUser: { id, updatedAt, resume, ...rest },
 }: {
   currentUser: NonNullable<RouterOutputs["user"]["getById"]>;
 }) {
@@ -58,7 +63,6 @@ export default function ProfileForm({
     },
     onError: async (error) => {
       await utils.user.getById.refetch(id, {}, { throwOnError: true });
-      console.log("hehehehe");
 
       // TODO: toast error message
     },
@@ -91,7 +95,100 @@ export default function ProfileForm({
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)} gap={"$4"}>
-      <ProfileFormInputs control={ctrl} />
+      <Controller
+        control={control}
+        name="firstName"
+        render={({ field }) => (
+          <Fieldset>
+            <Label disabled={field.disabled}>First name</Label>
+            <Input
+              value={field.value}
+              onBlur={field.onBlur}
+              onChangeText={field.onChange}
+              disabled={field.disabled}
+              disabledStyle={{ opacity: 0.5 }}
+              h={"$5"}
+              bw={0}
+            />
+          </Fieldset>
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="lastName"
+        render={({ field }) => (
+          <Fieldset>
+            <Label disabled={field.disabled}>Last name</Label>
+            <Input
+              value={field.value}
+              onBlur={field.onBlur}
+              onChangeText={field.onChange}
+              disabled={field.disabled}
+              disabledStyle={{ opacity: 0.5 }}
+              h={"$5"}
+              bw={0}
+            />
+          </Fieldset>
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="phoneNumber"
+        rules={{
+          validate: (validate) =>
+            validate.length > 0 ? isMobilePhone(validate) : true,
+        }}
+        render={({ field, fieldState: { error } }) => (
+          <Fieldset>
+            <Label disabled={field.disabled}>Phone number</Label>
+            <Input
+              value={field.value}
+              onBlur={field.onBlur}
+              onChangeText={field.onChange}
+              disabled={field.disabled}
+              disabledStyle={{ opacity: 0.5 }}
+              keyboardType="phone-pad"
+              h={"$5"}
+              bw={0}
+            />
+
+            {error && (
+              <SizableText color={"$red5"}>Invalid phone number.</SizableText>
+            )}
+          </Fieldset>
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: true,
+          validate: (validate) => isEmail(validate),
+        }}
+        render={({ field, fieldState: { error } }) => (
+          <Fieldset>
+            <Label disabled={true}>
+              Email address
+              <Asterisk scale={0.7} color={"$red10"} />
+            </Label>
+            <Input
+              value={field.value}
+              onBlur={field.onBlur}
+              onChangeText={field.onChange}
+              disabled={true}
+              disabledStyle={{ opacity: 0.5 }}
+              h={"$5"}
+              bw={0}
+            />
+            {error && (
+              <SizableText color={"$red5"}>Invalid email address.</SizableText>
+            )}
+          </Fieldset>
+        )}
+      />
 
       <YStack>
         <Label>Skills</Label>
