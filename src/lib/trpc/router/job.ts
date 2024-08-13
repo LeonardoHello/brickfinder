d73cfa62c8cc9f849eee153dd9ba29c738e37711
Leonardo <leonardo.yakub@gmail.com>
2024-08-13 17:46:35 +0200

    formatting

--- src/lib/trpc/router/job.ts
+++ src/lib/trpc/router/job.ts
@@ -2,46 +2,46 @@ import { publicProcedure, router } from "../init";
 import { ApplicationSchema, JobSchema } from "@/db/schema";
 
 export const jobRouter = router({
-	getAll: publicProcedure.query(({ ctx }) => {
-		return ctx.db.query.jobs.findMany({
-			columns: {
-				id: true,
-				createdAt: true,
-				expiresAt: true,
-				location: true,
-				title: true,
-				salary: true,
-			},
-		});
-	}),
-	getAllByUserId: publicProcedure
-		.input(ApplicationSchema.shape.userId)
-		.query(({ input, ctx }) => {
-			return ctx.db.query.jobs.findMany({
-				columns: {
-					id: true,
-					createdAt: true,
-					expiresAt: true,
-					location: true,
-					title: true,
-					salary: true,
-				},
-				with: {
-					company: { columns: { name: true } },
-					applications: {
-						columns: {
-							userId: true,
-							jobId: true,
-						},
-						where: (application, { eq }) => eq(application.userId, input),
-					},
-				},
-			});
-		}),
-	getById: publicProcedure.input(JobSchema.shape.id).query(({ input, ctx }) => {
-		return ctx.db.query.jobs.findFirst({
-			where: (job, { eq }) => eq(job.id, input),
-			with: { company: true },
-		});
-	}),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.query.jobs.findMany({
+      columns: {
+        id: true,
+        createdAt: true,
+        expiresAt: true,
+        location: true,
+        title: true,
+        salary: true,
+      },
+    });
+  }),
+  getAllByUserId: publicProcedure
+    .input(ApplicationSchema.shape.userId)
+    .query(({ input, ctx }) => {
+      return ctx.db.query.jobs.findMany({
+        columns: {
+          id: true,
+          createdAt: true,
+          expiresAt: true,
+          location: true,
+          title: true,
+          salary: true,
+        },
+        with: {
+          company: { columns: { name: true } },
+          applications: {
+            columns: {
+              userId: true,
+              jobId: true,
+            },
+            where: (application, { eq }) => eq(application.userId, input),
+          },
+        },
+      });
+    }),
+  getById: publicProcedure.input(JobSchema.shape.id).query(({ input, ctx }) => {
+    return ctx.db.query.jobs.findFirst({
+      where: (job, { eq }) => eq(job.id, input),
+      with: { company: true },
+    });
+  }),
 });
